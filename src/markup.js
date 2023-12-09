7b71ae53df017dd723ce01324220f9280dc3df88
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-12-09 03:59:14 +0200

    2

---
+++ src/markup.js
@@ -0,0 +1,50 @@
+
+
+.modal {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    /*background-color: rgba(255, 255, 255, 0.9); */
+  }
+  
+  .modal-content {
+    background-color: white;
+    margin: 20% auto;
+    padding: 20px;
+    width: 70%;
+    max-width: 600px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+  
+  .modal-header h2 {
+    margin-top: 0;
+    color: black; 
+  }
+  
+  .modal-body p {
+    color: black;
+  }
+  
+  .modal-body img {
+    width: 100%;
+    height: auto;
+    display: block;
+    margin-top: 10px;
+    border-radius: 5px;
+  }
+
+
+
+  .close-icon {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 20px; /* Встановіть потрібний розмір */
+    height: 20px; /* Встановіть потрібний розмір */
+    cursor: pointer;
+    color: #000;
+  }
