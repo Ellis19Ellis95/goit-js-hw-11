d6ed1b733abf67a615414206494b5ce9ba0afcbd
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-12-11 05:11:32 +0200

    1

--- src/save/modal-subscription.js
+++ src/save/modal-subscription.js
@@ -1,36 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const modalButtons = document.querySelectorAll('[data-modal-close]');
-    const modal1 = document.querySelector('.is-hidden-1');
-    const modal2 = document.querySelector('.is-hidden-2');
-    const form = document.querySelector('.footer-form');
-
-    const enteredEmails = new Map(); // Об'єкт для зберігання електронних адрес
-
-    function isEmailRegistered(email) {
-        return enteredEmails.has(email.trim().toLowerCase());
-    }
-
-    function handleRegistration(email) {
-        const lowerCasedEmail = email.trim().toLowerCase();
-        if (!isEmailRegistered(lowerCasedEmail)) {
-            enteredEmails.set(lowerCasedEmail, true);
-            modal1.classList.remove('is-hidden-1');
+document.addEventListener("DOMContentLoaded", function() {
+    var registeredEmails = []; 
+    
+    var modal = document.querySelector('[data-modal]');
+    var closeButton = modal.querySelector('[data-modal-close]');
+    var form = document.querySelector('.form-footer');
+    var newModal = document.querySelector('.backdrop');
+  
+    var closeButton2 = document.querySelector('.backdrop.is-hidden-2 [data-modal-close]');
+    var newModal2 = document.querySelector('.backdrop.is-hidden-2');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      var emailInput = form.querySelector('.input-label').value.toLowerCase(); 
+      if (!registeredEmails.includes(emailInput)) { 
+        registeredEmails.push(emailInput); 
+        newModal.classList.remove('is-hidden-1');
+      } else {
+        if (newModal2.classList.contains('is-hidden-1')) {
+          newModal2.classList.remove('is-hidden-1');
         } else {
-            modal2.classList.remove('is-hidden-2');
+          newModal2.classList.remove('is-hidden-2');
         }
-    }
-
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const email = e.target.querySelector('.footer-input').value;
-        handleRegistration(email);
+      }
     });
-
-    modalButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            const modal = this.closest('[data-modal]');
-            modal.classList.add('is-hidden-1');
-            modal.classList.add('is-hidden-2');
-        });
+  
+    closeButton.addEventListener('click', function() {
+      newModal.classList.add('is-hidden-1');
     });
-});
+  
+    closeButton2.addEventListener('click', function() {
+      newModal2.classList.add('is-hidden-2');
+    });
+  });
